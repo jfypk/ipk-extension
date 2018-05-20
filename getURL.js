202404650ed0f1affbf135d400e734d43d93b829
jpark11 <jypark03@gmail.com>
2018-05-20 15:52:18 -0400

    first commit

---
+++ getURL.js
@@ -0,0 +1,15 @@
+if(chrome.tabs != undefined) {
+	chrome.tabs.query({
+    active: true,
+    currentWindow: true
+}, function(tabs) {
+    var tabURL = tabs[0].url;
+    tabURL = tabURL.replace('http://','').replace('https://','').split(/[/?#]/)[0]; 
+    //check if there is a www. or not first
+    var firstDotIndex = tabURL.indexOf(".");
+    tabURL = tabURL.substr(firstDotIndex+1); // eslint-disable-line 
+    //else serve tabURL
+    document.getElementById("url").innerHTML = tabURL;
+    console.log("Domain: " + tabURL);
+});
+}
---
+++ todo.md
@@ -0,0 +1,7 @@
+#Todo
+
+[] Get URL reading to work
+[] Get permissions to work for chrome extension
+[] Build database
+[] Look and feel of website
+[] Include content on chrome extension
