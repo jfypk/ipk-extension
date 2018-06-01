704ebd9c81d9ebd11c9eadaf50651b3c881a2518
jpark11 <jypark03@gmail.com>
2018-06-01 19:39:28 -0400

    edited model schema

--- model/model.js
+++ model/model.js
@@ -1,3 +1,29 @@
 'use strict';
 
+//import dependency
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//create new instance of mongoose.scehma. 
+
+var modelSchema = new Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	name: String,
+	type: String, 
+	overall_score: Number,
+	energy_use: Number,
+	renewable_source: Number,
+	energy_usage: Number,
+	supply_chain: Number,
+	local_suppliers: Number,
+	organic_ingredients: Number,
+	waste_management: Number,
+	composting: Number,
+	recycling: Number,
+	food_waste: Number,
+	labor: Number,
+	fair_wage: Number,
+	diversity: Number
+} {collection: 'ipx'});
+
+module.exports = mongoose.model('model', modelSchema);
